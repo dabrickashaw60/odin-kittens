36adab610700bc8f33470ceb23788df6bc292706
dabrickashaw60 <121248625+dabrickashaw60@users.noreply.github.com>
2024-07-17 14:49:17 -0400

    Added Api

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,63 @@
+class KittensController < ApplicationController
+  before_action :set_kitten, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @kittens = Kitten.all
+    respond_to do |format|
+      format.html
+      format.json { render json: @kittens }
+end
+end
+  def show
+    respond_to do |format|
+      format.html
+      format.json { render json: @kitten }
+    end
+  end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def edit
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+
+    if @kitten.save
+      flash[:success] = 'Kitten was successfully created! Meow!'
+      redirect_to @kitten
+    else
+      flash.now[:error] = 'Oops! There was an error creating the kitten. Please check your form.'
+      render :new
+    end
+  end
+
+  def update
+    if @kitten.update(kitten_params)
+      flash[:success] = 'Kitten was successfully updated! Purrrfect!'
+      redirect_to @kitten
+    else
+      flash.now[:error] = 'Uh-oh! There was an error updating the kitten. Please check your form.'
+      render :edit
+    end
+  end
+
+  def destroy
+    @kitten.destroy
+    flash[:success] = 'Kitten was successfully deleted! Goodbye, little one.'
+    redirect_to kittens_url
+  end
+
+  private
+  def set_kitten
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+  end
+
+end
+
